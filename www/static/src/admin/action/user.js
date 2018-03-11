0bfb4f0ec983b1da37c09bd9ef318b1e98afb49b
zxysilent <zxysilent@foxmail.com>
2018-03-11 11:34:34 +0800

    back static

---
+++ www/static/src/admin/action/user.js
@@ -0,0 +1,11 @@
+import Reflux from 'reflux';
+
+export default Reflux.createActions({
+  select: {children: ['completed', 'failed']},
+  delete: {children: ['completed', 'failed']},
+  pass: {children: ['completed', 'failed']},
+  save: {children: ['completed', 'failed'], asyncResult: true},
+  savepwd: {children: ['completed', 'failed'], asyncResult: true},
+  login: {children: ['completed', 'failed'], asyncResult: true},
+  generateKey: {children: ['completed', 'failed'], asyncResult: true}
+});
